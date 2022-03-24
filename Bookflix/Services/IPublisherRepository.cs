4059088ac8ccc5999f4503a49ca6cd388ff3e8a0
mohamed gamal <midogemy100@gmail.com>
2022-03-24 23:05:27 +0200

    add services folder

---
+++ Bookflix/Services/IPublisherRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bookflix.Models;
+
+namespace Bookflix.Services
+{
+    public interface IPublisherRepository
+    {
+        public List<Publisher> GetAll();
+        public Publisher GetDetails(int id);
+        public void Insert(Publisher pub);
+        public void UpdatePub(int id, Publisher pub);
+        public void DeletePub(int id);
+    }
+}
---
+++ Bookflix/Services/PublisherRepoService.cs
@@ -0,0 +1,46 @@
+﻿using Bookflix.Models;
+using Bookflix.Models.Context;
+
+namespace Bookflix.Services
+{
+    public class PublisherRepoService: IPublisherRepository
+    {
+        public BookflixDbContext Context { get; set; }
+
+        public PublisherRepoService(BookflixDbContext context)
+        {
+            Context = context;
+        }
+
+        public List<Publisher> GetAll()
+        {
+            return Context.Publishers.ToList();
+        }
+
+        public Publisher GetDetails(int id)
+        {
+            return Context.Publishers.Find(id);
+        }
+
+        public void Insert(Publisher pub)
+        {
+            Context.Publishers.Add(pub);
+            Context.SaveChanges();
+        }
+
+        public void UpdatePub(int id, Publisher pub)
+        {
+            Publisher pubUpdated = Context.Publishers.Find(id);
+            pubUpdated.Name = pub.Name;
+            pubUpdated.Address = pub.Address;
+
+            Context.SaveChanges();
+        }
+
+        public void DeletePub(int id)
+        {
+            Context.Remove(Context.Publishers.Find(id));
+            Context.SaveChanges();
+        }
+    }
+}
