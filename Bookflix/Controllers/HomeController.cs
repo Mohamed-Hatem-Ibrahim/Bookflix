69a94588be03dee3dafd8997f841a71ec77fa4e1
Mohamed <mohamedhatemibrahim@gmail.com>
2022-04-09 13:15:16 +0200

    fixed some mistakes/ typos

--- Bookflix/Controllers/HomeController.cs
+++ Bookflix/Controllers/HomeController.cs
@@ -34,20 +34,15 @@ namespace Bookflix.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult About()
-        {
-            return View();
         }
     }
 }
