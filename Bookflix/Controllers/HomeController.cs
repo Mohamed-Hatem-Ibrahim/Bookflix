2b80ae8a0fcf5c3424c14d30989c930384b10ca4
Mina-Alphonse <mina_alphonse@outlook.com>
2022-04-09 14:06:37 +0200

    buttons style

--- Bookflix/Controllers/HomeController.cs
+++ Bookflix/Controllers/HomeController.cs
@@ -44,10 +44,5 @@ namespace Bookflix.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public IActionResult About()
-        {
-            return View();
-        }
     }
 }
